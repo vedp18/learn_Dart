d843cbcd2749d8e9dd3d061255ee1a2fb26c7cfa
Ved Patel <ved09003@gmail.com>
2025-07-05 10:38:05 +0530

    corrected getter and setter in encapsulation_oop.dart

--- OOP/encapsulation_oop.dart
+++ OOP/encapsulation_oop.dart
@@ -1,16 +1,13 @@
-/// Encapsulation is bundling data(variables) and methods into capsule or single unit. 
+/// Encapsulation is bundling data(variables) and methods into capsule or single unit.
 /// Making data only accessible through methods only instead of direct access.
 /// achieved using private variables (_<varName>) and getters & setters
 
 class A {
   int _var1 = 10;
-  
-  int getVar1(){
-    return _var1;
-  }
 
-  void setVar1(int var1) => this._var1 = var1;
+  int get var1 => _var1;
 
-} 
+  set var1(int var1) => _var1 = var1;
+}
 
 /// now _var1 is private variable cannot be accessed directly outside this library, only accessible using getVar1() method only.
