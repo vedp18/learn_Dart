a839f92432aec2879b771b51f9d4c475d8707301
Ved Patel <ved09003@gmail.com>
2025-07-05 10:30:18 +0530

    Update class_object.dart
    updated getter and setter methods

--- OOP/class_object.dart
+++ OOP/class_object.dart
@@ -19,35 +19,41 @@ class NinjaTeam {
 
   /// getters
 
-  String getTeamName() => this._teamName;
+  // String getTeamName() => this._teamName;
+  String get teamName => _teamName;
 
-  String getMem1() => this._mem1;
+  // String getMem1() => this._mem1;
+  String get mem1 => _mem1;
   
-  String getMem2() => this._mem2;
+  // String getMem2() => this._mem2;
+  String get mem2 => _mem2;
   
-  String getMem3() => this._mem3;
+  // String getMem3() => this._mem3;
+  String get mem3 => _mem3;
 
-  int getTeamNumber() => this._teamNumber;
+  // int getTeamNumber() => this._teamNumber;
+  int get teamNumber => _teamNumber;
 
 
   /// setters
 
-  void setTeamName(String teamName) => this._teamName = teamName;
+  // void setTeamName(String teamName) => this._teamName = teamName;
+  set teamName(String teamName) => _teamName = teamName;
 
-  void setMem1(String mem1) => this._mem1 = mem1;
+  set mem1(String mem1) => _mem1 = mem1;
 
-  void setMem2(String mem2) => this._mem2 = mem2;
+  set mem2(String mem2) => _mem2 = mem2;
 
-  void setMem3(String mem3) => this._mem3 = mem3;
+  set mem3(String mem3) => _mem3 = mem3;
 
-  void setTeamNumber(int teamNumber) => this._teamNumber = teamNumber;
+  set teamNumber(int teamNumber) => _teamNumber = teamNumber;
 
 
 }
 
 void main(List<String> args) {
   NinjaTeam team1 = NinjaTeam("Team Kakkashi", "Naruto Uzumaki", "Sakura Harunai", "Sasuke Uchiha", 007);
-  team1._mem1 = "Gaara";
-  print(team1._mem1);
+  team1.mem1 = "Gaara";
+  print(team1.mem1);
 
 }
